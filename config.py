a143f275bca0af96c8c86e488adadaccbf4bc551
SanyaSteam <s40120665@gmail.com>
2026-08-14 15:29:56 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Все интернет-адреса и конфигурация бота в одном месте
+
+# 1. Адрес для запроса скидок у Steam (Казахстан)
+STEAM_API_URL="https://store.steampowered.com/api/featuredcategories/?cc=kz&l=ru"
+
+# 2. Базовый адрес для открытия страниц игр в браузере
+STEAM_STORE_APP_BASE="https://store.steampowered.com/app/"
+
+# 3. Базовый адрес для отправки сообщений через API Telegram
+TELEGRAM_API_BASE="https://api.telegram.org/bot"
