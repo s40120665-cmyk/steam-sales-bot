53b27d7b84bc8ebf149aec5642ab715662765fc1
SanyaSteam <s40120665@gmail.com>
2026-08-14 15:39:31 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,6 @@ STEAM_STORE_APP_BASE="https://store.steampowered.com/app/"
 
 # 3. Базовый адрес для отправки сообщений через API Telegram
 TELEGRAM_API_BASE="https://api.telegram.org/bot"
+
+# 4. Базовый адрес для запроса цен по списку конкретных ID игр
+STEAM_PRICE_API_BASE="https://store.steampowered.com/api/appdetails?appids="
